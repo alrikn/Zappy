d3d62262cd8eb8a09d1a5742ad75ff32657b44ec
Alrik Neihouser <alrik.neihouser@epitech.eu>
2026-05-28 09:53:42 +0200

    feat: population of resources

--- Server/include/Struct.hpp
+++ Server/include/Struct.hpp
@@ -9,6 +9,15 @@
     #define INCLUDED_STRUCT_HPP
 
 
+//on a 10 by 10 world there would be 50 food and 5 thystane
+const float FOOD_DENSITY = 0.5;
+const float LINEMATE_DENSITY = 0.3;
+const float DERAUMERE_DENSITY = 0.15;
+const float SIBUR_DENSITY = 0.1;
+const float MENDIANE_DENSITY = 0.1;
+const float PHIRAS_DENSITY = 0.08;
+const float THYSTAME_DENSITY = 0.05;
+
 typedef struct resources
 {
     int food = 0; //food also represents lifespan of the player, if it reaches 0 the player dies
