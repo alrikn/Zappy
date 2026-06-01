1453585947fef6a5168b1bd14660836616bdd86a
JadBGHIEL <jad.bghiel@epitech.eu>
2026-06-01 22:06:02 +0200

    add: parsing args

--- Ai/main.py
+++ Ai/main.py
@@ -4,3 +4,35 @@
 ## File description:
 ## entry point, parse args and launch the ai
 ##
+
+import argparse
+import sys
+from connection import Connection
+from player import PlayerAI
+
+
+def parse_args() -> tuple[int, str, str]:
+    # using argparse here, way cleaner than doing it manually
+    # https://docs.python.org/3/library/argparse.html
+    parser = argparse.ArgumentParser(
+        prog="zappy_ai",
+        usage="./zappy_ai -p port -n name -h machine",
+        add_help=False,
+    )
+    parser.add_argument("-p", type=int, dest="port", required=True)
+    parser.add_argument("-n", type=str, dest="name", required=True)
+    parser.add_argument("-h", type=str, dest="machine", default="localhost")
+    parser.add_argument("--help", action="help")
+
+    try:
+        args = parser.parse_args()
+    except SystemExit:
+        sys.exit(84)
+
+    # basic check on the port
+    if args.port <= 0 or args.port > 65535:
+        print("USAGE: ./zappy_ai -p port -n name -h machine", file=sys.stderr)
+        sys.exit(84)
+
+    return args.port, args.name, args.machine
+
