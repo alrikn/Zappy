e3ec39b11c9b1b6f335d11f3e900b46a3d8d471e
Alrik Neihouser <alrik.neihouser@epitech.eu>
2026-06-15 09:54:00 +0200

    feat: a few updates to the player

--- Server/src/Clients/Gui/GuiPassive.cpp
+++ Server/src/Clients/Gui/GuiPassive.cpp
@@ -4,6 +4,23 @@
 ** File description:
 ** GuiPassive
 ** This file is for all the functions that the server sends to the gui without explicit request from the gui.
+** These functions will jnot handle throwing the message to evry gui connection, they only focus on themselves
 */
 
 #include "Gui.hpp"
+#include "Player.hpp"
+
+//connection of a new player
+//returns: pnw <player id> <x> <y> <orientation> <level> <team name>
+void Gui::pnw(Server &server, std::shared_ptr<Player> player)
+{
+    std::string result = "pnw";
+    
+    result += " " + std::to_string(player->getId());
+    result += " " + std::to_string(player->getX());
+    result += " " + std::to_string(player->getY());
+    result += " " + std::to_string(player->getOrientation());
+    result += " " + std::to_string(player->getLevel());
+    result += " " + player->getTeamName();
+    send_message(result);
+}
