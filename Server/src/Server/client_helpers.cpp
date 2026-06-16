fbcd27214716f90d55df7cb8202a0a05cbbe4912
JadBGHIEL <jad.bghiel@epitech.eu>
2026-06-16 09:47:11 +0200

    rm: notify gui

--- Server/src/Server/client_helpers.cpp
+++ Server/src/Server/client_helpers.cpp
@@ -23,11 +23,6 @@ void Server::move_player(Player &player, int new_x, int new_y)
     if (it != _clients.end())
         _map[new_y][new_x].players.push_back(
             std::dynamic_pointer_cast<Player>(it->second));
-    // orientation is 1 to 4 in the protocol
-    notify_gui("ppo " + std::to_string(player.getId())
-        + " " + std::to_string(new_x)
-        + " " + std::to_string(new_y)
-        + " " + std::to_string(static_cast<int>(player.orientation) + 1) + "\n");
 }
 
 Resource parse_resource(const std::string& name)
